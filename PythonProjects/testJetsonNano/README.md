b7de1ff10d3d4505c545ef28c4a34f3b535afca7
Eduardo Zarate <edu.zc07@gmail.com>
2020-04-07 08:17:45 -0500

    JetsonNano updated

---
+++ PythonProjects/testJetsonNano/README.md
@@ -0,0 +1,27 @@
+# Test Jetson Nano
+
+Check Image Size
+```
+v4l2-ctl --list-formats-ext
+```
+
+
+```
+sudo jetson_clocks
+```
+
+```
+some_command & disown
+```
+
+```
+systemctl enable --now atd.service
+```
+
+```
+echo "shotCamera" | at 05:00 AM
+```
+
+```
+rsync -avz edu@192.168.1.14:/home/edu/Workspace/pythonExamples/videos/07042020 ~/jetson_nano_videos/
+```
--- PythonProjects/testJetsonNano/timeLapsed.py
+++ PythonProjects/testJetsonNano/timeLapsed.py
@@ -19,22 +19,23 @@ print(opt)
 # create display window
 #frame_width = 640
 #frame_height = 480
-width = [1280 640]
-height = [720 480]
-frame_width = width[0]
-frame_height = height[0]
+width = [1920, 1280, 1024, 640, 800, 1280, 320]
+height = [1080, 720, 768, 480, 600, 1024, 240]
+frame_width = width[1]
+frame_height = height[1]
 
 # create camera device
 camera = jetson.utils.gstCamera(frame_width, frame_height, "/dev/video0")
 
 dT = datetime.datetime.now()
-timestampStr = dT.strftime("%H%M_%d%m%Y")
+timestampName = dT.strftime("%d%m%Y_%H%M%S")
+folderDate = dT.strftime("%d%m%Y")
 
 #cv2.VideoWriter_fourcc('M','J','P','G'),
 
 fps = opt.fps
 # Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
-out = cv2.VideoWriter('videos/timeLapsed_%s.mp4'%(timestampStr),
+out = cv2.VideoWriter('/home/edu/Workspace/pythonExamples/videos/%s/%s_tlapsed.mp4'%(folderDate, timestampName),
                       cv2.VideoWriter_fourcc(*'mp4v'),
                       fps,
                       (frame_width, frame_height))
@@ -45,10 +46,10 @@ camera.Open()
 #Create array for Images
 #images_array = []
 
-time.sleep(5)
+time.sleep(1)
 
 max_frames = round(300 * opt.hours)
-num_frames = 120 #100 -> 10 seg // 5 Frames per Minutes
+num_frames = 120 #100 -> 10 seg // 120 -> 5 Frames per Minutes
 frames = 0
 count = 0
 
@@ -63,7 +64,7 @@ try:
             # Converting datetime object to string
             dateTimeObj = datetime.datetime.now()
             timestampStr = dateTimeObj.strftime("%H:%M:%S_%d/%m/%Y")
-            print "Frame saved: %s - %s"%(count, timestampStr)
+            print "Frame saved: %s/%d - %s"%(count, max_frames, timestampStr)
             jetson.utils.cudaDeviceSynchronize()
             arr = jetson.utils.cudaToNumpy(image, width, height, 4)
             img_data = cv2.cvtColor(arr, cv2.COLOR_RGBA2RGB).astype(np.uint8)
@@ -72,9 +73,12 @@ try:
             #images_array.append(img_data)
 except:
     print("\n ---- Closing ---")
+    print('Saved video in /home/edu/Workspace/pythonExamples/videos/%s/%s_tlapsed.mp4'%(folderDate, timestampName))
     camera.Close()
     out.release()
 
+print("\n ---- Closing ---")
+print('Saved video in /home/edu/Workspace/pythonExamples/videos/%s/%s_tlapsed.mp4'%(folderDate, timestampName))
 # close the camera
 camera.Close()
 out.release()
