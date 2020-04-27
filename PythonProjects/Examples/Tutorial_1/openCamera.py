e70910ce03d7ce24dd47ce596349f06e73a55f8e
Eduardo Zarate <edu.zc07@gmail.com>
2020-04-26 19:37:53 -0500

    update. New program added.

--- PythonProjects/Examples/Tutorial_1/openCamera.py
+++ PythonProjects/Examples/Tutorial_1/openCamera.py
@@ -1,20 +1,38 @@
 import numpy as np
 import cv2
+import sys
 
-cap = cv2.VideoCapture(0)
+def open_camera(cap):
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+        # Our operations on the frame come here
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # Display the resulting frame
+        cv2.imshow('frame',gray)
+        key = cv2.waitKey(1)
+        c = chr(key & 255)
+        if c in ['q', 'Q', chr(27)]:
+            break
 
-    # Display the resulting frame
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    print(__doc__)
+
+    if len(sys.argv) == 1:
+        cap = cv2.VideoCapture(0)
+    else:
+        cap = cv2.VideoCapture(sys.argv[1])
+        if not cap.isOpened():
+            cap = cv2.VideoCapture(int(sys.argv[1]))
+
+    if not cap.isOpened():
+        print('Cannot initialize video capture')
+        sys.exit(-1)
+
+    open_camera(cap)
